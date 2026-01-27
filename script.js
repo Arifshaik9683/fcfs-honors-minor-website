afcb879d9c2ad9ab94c746da91d0565d49f6e3a7
Arifshaik9683 <arifshaik9683@gmail.com>
2026-01-27 22:31:48 +0530

    Add files via upload

---
+++ script.js
@@ -0,0 +1,42 @@
+let queue = [];
+
+function bookTicket() {
+    let name = document.getElementById("name").value;
+
+    if (name === "") {
+        alert("Please enter your name");
+        return;
+    }
+
+    queue.push(name); // FCFS add
+    document.getElementById("msg").innerText =
+        name + " booked ticket. Queue Number: " + queue.length;
+
+    document.getElementById("name").value = "";
+    displayQueue();
+}
+
+function serveCustomer() {
+    if (queue.length === 0) {
+        alert("No customers in queue");
+        return;
+    }
+
+    let served = queue.shift(); // FCFS serve
+    alert("Now Serving: " + served);
+    displayQueue();
+}
+
+function displayQueue() {
+    let list = document.getElementById("queueList");
+    list.innerHTML = "";
+
+    queue.forEach((customer, index) => {
+        let li = document.createElement("li");
+        li.innerText = (index + 1) + ". " + customer;
+        list.appendChild(li);
+    });
+}
+function toggleDarkMode() {
+    document.body.classList.toggle("dark");
+}
